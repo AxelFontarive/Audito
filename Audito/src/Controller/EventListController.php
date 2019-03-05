de04a94684b1b6b0adb78854cdeccc496b4243bb
Axel FONTARIVE <afontarive@gmail.com>
2019-03-05 10:32:24 +0100

    Ajout href/css sur homepage + routes perso pour listes evenements

---
+++ Audito/src/Controller/EventListController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventListController extends AbstractController
+{
+    public function list($slug)
+    {
+        return $this->render('event_list/index.html.twig', [
+            'slug' => $slug,
+        ]);
+    }
+
+    public function all()
+    {
+        return $this->render('event_list/index.html.twig', [
+            'slug' => "all",
+        ]);
+    }
+}
