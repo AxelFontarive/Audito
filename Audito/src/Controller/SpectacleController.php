83a21f24fb5586fd7151305ccc7e662babdde891
Axel FONTARIVE <afontarive@gmail.com>
2019-03-15 16:33:14 +0100

    Ajout spectacle en db (form) et listing

--- Audito/src/Controller/SpectacleController.php
+++ Audito/src/Controller/SpectacleController.php
@@ -4,50 +4,58 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Spectacle;
+use App\Entity\TypeSpectacle;
 
 class SpectacleController extends AbstractController
 {
     public function adminList(EntityManagerInterface $iface)
     {
-        // Check if a user is connected
-        if($this->getUser())
-        {
-            // Access if the user is an admin
-            if(in_array("ROLE_ADMIN",$this->getUser()->getRoles()))
-            {
-                $repo = $iface->getRepository(Spectacle::class);
-                $sp = $repo->findAll();
+        $repo = $iface->getRepository(Spectacle::class);
+        $sp = $repo->findAll();
 
-                return $this->render('spectacle/adminlist.html.twig', [
-                    "auth" => true,
-                    "admin" => true,
-                    "len" => count($sp),
-                    "spec" => $sp,
-                ]);
+        return $this->render('spectacle/adminlist.html.twig', ["len" => count($sp), "sp" => $sp]);
+    }
+
+    public function add(EntityManagerInterface $iface, Request $request)
+    {
+        $repo = $iface->getRepository(TypeSpectacle::class);
+        $req = $request->request;
+        $token = $request->request->get('token');
+        
+        if($token)
+        {
+            //dd("TODO NO CHECKING HERE !");
+            $spec = new Spectacle();
+            $spec->setNomSpectacle($req->get('sp_name'));
+            $spec->setDescription($req->get('sp_desc'));
+
+            $type = $repo->find($req->get('sp_type'));
+            $spec->setTypeSpectacle($type);
+            $spec->setImage($req->get('sp_url'));
+            if($req->get('sp_scenemod'))
+            {
+                $spec->setModulationScene(true);
             }
             else
             {
-                // The user is not an admin
-                return $this->render('spectacle/adminlist.html.twig', [
-                    "auth" => true,
-                    "admin" => false,
-                ]);                
+                $spec->setModulationScene(false);
             }
+
+            $iface->persist($spec);
+            $iface->flush();
+
+            // Returning to the lists CHANGE THIS
+            return $this->render('home/index.html.twig', []);
         }
         else
         {
-            // The user isn't connected
-            return $this->render('spectacle/adminlist.html.twig', [
-                "auth" => false,
-                "admin" => false,
-            ]); 
+            // Getting the TypeSpectacle objects
+            $tsp = $iface->getRepository(TypeSpectacle::class)->findAll();
+            return $this->render('spectacle/adminadd.html.twig', ["tsp" => $tsp]);
         }
-    }
-
-    public function add(EntityManagerInterface $iface)
-    {
-        return $this->render('spectacle/adminadd.html.twig', []);
     }
 }
